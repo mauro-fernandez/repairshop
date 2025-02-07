e79c429580eb0c9e8ac8a49cfcf25a73b704a30d
Mauro Fernandez <mauro.e.n.fernandez@gmail.com>
2025-02-07 18:08:06 -0300

    kinde sdk, login and logout integration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
-import { HomeIcon, File, UsersRound } from "lucide-react"
+import { HomeIcon, File, UsersRound, LogOut } from "lucide-react"
 import Link from "next/link"
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
+
+import { Button } from "@/components/ui/button"
 import { NavButton } from "@/components/NavButton"
-//import { ModeToggle } from "@/components/ModeToggle"
-import { ModeToggle } from "./ModeToggle"
+import { ModeToggle } from "@/components/ModeToggle"
+//import { ModeToggle } from "./ModeToggle"
 
 export function Header(){
     return (
@@ -20,6 +23,12 @@ export function Header(){
                     <NavButton href="/tickets" label="Tickets" icon={File} />
                     <NavButton href="/customers" label="Customers" icon={UsersRound} />
                     <ModeToggle />
+
+                    <Button variant='ghost' size='icon' aria-label="LogOut" title="LogOut" className="rounded-full" asChild >
+                        <LogoutLink>
+                            <LogOut />
+                        </LogoutLink>
+                    </Button>
                 </div>
             </div>
 
